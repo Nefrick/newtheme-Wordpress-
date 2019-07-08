9ef4eed3b73c8ebbb843d954c3c17e11769d6835
Nefrick <it.nefrick@gmail.com>
2019-07-08 22:33:42 +0300

    Filter WP_Query WHERE

---
+++ plugins/hello.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 1.7.2
+ */
+/*
+Plugin Name: Hello Dolly
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Matt Mullenweg
+Version: 1.7.2
+Author URI: http://ma.tt/
+*/
+
+add_filter( 'posts_where', 'modify_posts_where' );
+
+function modify_posts_where( $where ){
+    $range = array( 'start' => 80, 'end' => 90 );
+    return $where . ' AND id >= ' . $range['start'] . ' AND id <= ' . $range['end'];
+
+}
+
+$query = new WP_Query([
+        'post_type'         => 'post',
+        'post_status'       => 'publish',
+        'posts_per_page'    => -1,
+        'fields'            => 'ids'
+    ]);
+
+print_r( $query->request );
+var_dump( $query->posts );
+die();
